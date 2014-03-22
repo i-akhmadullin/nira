bc655474270c22f60f94851b01d8e95023f0a814
Ilia Akhmadullin <rafoex@yandex-team.ru>
2014-03-22 17:08:05 +0600

    add basic jade templating

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-var koa = require('koa');
+var render = require('./lib/render');
 var logger = require('koa-logger');
+var route = require('koa-route');
+var koa = require('koa');
 var app = koa();
 
-// logger
 app.use(logger());
 
-// response
-app.use(function *(){
-  this.body = 'Hello World';
-});
+app.use(route.get('/', list));
+
+/**
+ * Post listing.
+ */
+var tasks = [{ title:'todo1', text: 'description' }, {title: 'todo2', text: 'description2\nline2' }]
+function *list() {
+  this.body = yield render('list', { tasks: tasks });
+}
 
 app.listen(3000);
 console.log('running on port 3000');
