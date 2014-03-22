ef4a20ed9e0e189477165a8184839f423d658023
Ilia Akhmadullin <rafoex@yandex-team.ru>
2014-03-23 00:02:10 +0600

    add koa-pg

--- index.js
+++ index.js
@@ -3,33 +3,21 @@ var logger = require('koa-logger');
 var route = require('koa-route');
 var config = require('nconf');
 var pg = require('pg');
+var koaPg = require('koa-pg');
 var koa = require('koa');
 var app = koa();
 
 config.argv().env()
     .file({ file: 'configs/' + process.env.NODE_ENV + '.json' });
+
+app.use(koaPg(config.get('db')));
 app.use(logger());
 app.use(route.get('/', list));
 
-/**
- * Post listing.
- */
-var tasks;
-
 function *list() {
-    pg.connect(config.get('db'), function(err, client, done) {
-        if(err) {
-            return console.error('error fetching client from pool', err);
-        }
-        client.query('SELECT * from tasks', function(err, result) {
-            //call `done()` to release the client back to the pool
-            done();
-            if(err) {
-                return console.error('error running query', err);
-            }
-            tasks = result.rows;
-        });
-    });
+    // Here we have access to this.pg which is client returned from pg.connect().
+    var result = yield this.pg.db.client.query_('SELECT * from tasks')
+    var tasks = result.rows;
     this.body = yield render('list', { tasks: tasks });
 }
 
