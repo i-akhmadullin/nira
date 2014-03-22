a1f25de9793ca8ea7e29d0d0b876e4fe58223235
Ilia Akhmadullin <rafoex@yandex-team.ru>
2014-03-22 21:44:15 +0600

    hello nconf

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 var render = require('./lib/render');
 var logger = require('koa-logger');
 var route = require('koa-route');
+var config = require('nconf');
+var pg = require('pg');
 var koa = require('koa');
 var app = koa();
 
+config.argv().env()
+    .file({ file: 'configs/' + process.env.NODE_ENV + '.json' });
 app.use(logger());
-
 app.use(route.get('/', list));
 
 /**
  * Post listing.
  */
-// var tasks = [{ title:'todo1', text: 'description' }, {title: 'todo2', text: 'description2\nline2' }]
 var tasks;
-var pg = require('pg');
-var conString = "postgres://postgres:postgres@localhost/nira";
-
 
 function *list() {
-    pg.connect(conString, function(err, client, done) {
+    pg.connect(config.get('db'), function(err, client, done) {
         if(err) {
             return console.error('error fetching client from pool', err);
         }
         client.query('SELECT * from tasks', function(err, result) {
             //call `done()` to release the client back to the pool
             done();
-
             if(err) {
                 return console.error('error running query', err);
             }
-            console.log(result.rows[0].numbor);
-            //output: 1
             tasks = result.rows;
         });
     });
