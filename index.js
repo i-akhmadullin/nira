e20a1d9d5267622c7a196f35eafe66db3d01e75b
Ilia Akhmadullin <rafoex@yandex-team.ru>
2014-03-22 20:24:15 +0600

    hello postgres

--- index.js
+++ index.js
@@ -11,9 +11,30 @@ app.use(route.get('/', list));
 /**
  * Post listing.
  */
-var tasks = [{ title:'todo1', text: 'description' }, {title: 'todo2', text: 'description2\nline2' }]
+// var tasks = [{ title:'todo1', text: 'description' }, {title: 'todo2', text: 'description2\nline2' }]
+var tasks;
+var pg = require('pg');
+var conString = "postgres://postgres:postgres@localhost/nira";
+
+
 function *list() {
-  this.body = yield render('list', { tasks: tasks });
+    pg.connect(conString, function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('SELECT * from tasks', function(err, result) {
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+                return console.error('error running query', err);
+            }
+            console.log(result.rows[0].numbor);
+            //output: 1
+            tasks = result.rows;
+        });
+    });
+    this.body = yield render('list', { tasks: tasks });
 }
 
 app.listen(3000);
